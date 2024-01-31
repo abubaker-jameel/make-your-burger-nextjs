3652b8e0b8f58933b0f07cec4a7f8dba29355592
Abubaker Jamil <codecookies958@gmail.com>
2024-01-31 22:59:05 +0500

    fix: fixed the footer padding

--- src/app/_containers/footer/RightFooterList.jsx
+++ src/app/_containers/footer/RightFooterList.jsx
@@ -3,14 +3,14 @@ import footerData from "@/app/_data/footerData";
 import Icon from "@/app/_components/Icons/Icon";
 
 const RightFooterList = () => {
-  const navItem = footerData.icons.map((item) => {
+  const rightFooterItem = footerData.icons.map((item) => {
     return (
       <li key={item.id} className="relative h-8 w-8 [&:not(:first-child)]:ml-2">
         <Icon src={item.source} alt={item.name} />
       </li>
     );
   });
-  return <ul className="flex items-center justify-end">{navItem}</ul>;
+  return <ul className="flex items-center justify-end">{rightFooterItem}</ul>;
 };
 
 export default RightFooterList;
