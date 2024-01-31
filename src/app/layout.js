0c8f2457b31791682be1b6f20853054c6ed44fb4
Abubaker Jamil <codecookies958@gmail.com>
2024-01-31 20:59:47 +0500

    fix: fixed the responsive of home page and their elements

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={openSans.className}>
-      <body>
+      <body className="flex min-h-screen flex-col justify-between">
         {
           <>
             <Header />
